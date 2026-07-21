78090d84e6b3b8bf4b701c08577ea1e9f7d37487
1juicef <josefubaka@gmail.com>
2026-07-21 21:51:59 +0200

    feat(01-03): lazy WebP screenshots, Floor 0 fix, Dispater trailer
    - Rewrite all 16 screenshot <img> tags (Drag Rush, Dispater, Floor 0, SwingSpace) to loading="lazy" before src="...webp", per Firefox attribute-order bug #1647077
    - Fix Floor 0's broken floor-0-1..4.png references, repointing to img/projects/floor-0/Floor0SC1-4.webp (satisfies CONT-01 early per D-14)
    - Add Dispater's missing YouTube trailer embed (video id ihPEcIQ_PwI) with loading="lazy", mirroring Drag Rush's existing trailer
    - Add loading="lazy" to Drag Rush's existing trailer iframe
    - Add preload="metadata" to SwingSpace's overlay video to defer the multi-MB clip fetch

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -14,24 +14,50 @@ export default [
         You gotta stick to the beat!
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/L5YWz2i434E" frameborder="0" allowfullscreen></iframe>
+        <iframe class="youtube" loading="lazy" src="https://www.youtube.com/embed/L5YWz2i434E" frameborder="0" allowfullscreen></iframe>
     </div>
     <div class="paragraph center">
-        <img class="pc-screenshot" src="img/projects/drag-rush-1.png" alt="Drag Rush Screenshot" />
-        <img class="pc-screenshot" src="img/projects/drag-rush-2.png" alt="Drag Rush Screenshot" />
-        <img class="pc-screenshot" src="img/projects/drag-rush-3.png" alt="Drag Rush Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/drag-rush/DragrushSC1.webp" alt="Drag Rush Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/drag-rush/DragrushSC2.webp" alt="Drag Rush Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/drag-rush/DragrushSC3.webp" alt="Drag Rush Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/drag-rush/DragrushSC4.webp" alt="Drag Rush Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/drag-rush/DragrushSC5.webp" alt="Drag Rush Screenshot" />
+    </div>
+    <div class="paragraph center">
+        <h3>About this game</h3>
+        Built in Unity (8 weeks).<br/>
+        Unique twist between a racing- and rythmgame.<br/>
+        Custom made "Beat Conductor".<br/>
+        Fast-paced, satisfying and a "ok, one more try!" hook.
     </div>
     <div class="paragraph center">
         <a href="https://yrgo.itch.io/drag-rush" target="_blank">Play on itch.io</a>
     </div>
     `, "#6C3BAA", false, false),
-    new ProjectData("dispater", "Dispater", "img/projects/dispater-1.png", `
+    new ProjectData("dispater", "Dispater", "img/projects/dispater/DispaterSC4.png", `
     <div class="paragraph">
-        <strong>Dispater</strong> is a sci-fi narrative game. Explore a mysterious facility and uncover the secrets within.
+        <strong>"Good morning rookie!"</strong>
+    </div>
+    <div class="paragraph">
+        Are you ready for your first day as the elevator operator at the D.I mining station. The station can be a bit cramped but you should have all you need to guide the rest of your crew through the mines. The Disparator Corporation welcomes you as the newest member of the Disparator family.
     </div>
     <div class="paragraph center">
-        <img class="pc-screenshot" src="img/projects/dispater-1.png" alt="Dispater Screenshot" />
-        <img class="pc-screenshot" src="img/projects/dispater-2.png" alt="Dispater Screenshot" />
+        <iframe class="youtube" loading="lazy" src="https://www.youtube.com/embed/ihPEcIQ_PwI" frameborder="0" allowfullscreen></iframe>
+    </div>
+    <div class="paragraph center">
+        <img class="pc-screenshot" loading="lazy" src="img/projects/dispater/DispaterSC1.webp" alt="Dispater Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/dispater/DispaterSC2.webp" alt="Dispater Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/dispater/DispaterSC3.webp" alt="Dispater Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/dispater/DispaterSC4.webp" alt="Dispater Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/dispater/DispaterSC5.webp" alt="Dispater Screenshot" />
+    </div>
+    <div class="paragraph center">
+        <h3>About this game</h3>
+        Made in Unreal Engine (8 weeks).<br/>
+        Heavy narrative.<br/>
+        Custom made hexagonal gridbase movement.<br/>
+        Atmospheric and creepy enviroment.<br/>
+        Ending will leave you in SHOCK.
     </div>
     <div class="paragraph center">
         <a href="https://yrgo.itch.io/dispater" target="_blank">Play on itch.io</a>
@@ -42,10 +68,40 @@ export default [
         <strong>Floor 0</strong> is an atmospheric exploration game set in a derelict underground facility. Navigate through abandoned corridors and discover the mysteries of what happened here.
     </div>
     <div class="paragraph center">
-        <img class="pc-screenshot" src="img/projects/floor-0-1.png" alt="Floor 0 Screenshot" />
-        <img class="pc-screenshot" src="img/projects/floor-0-2.png" alt="Floor 0 Screenshot" />
-        <img class="pc-screenshot" src="img/projects/floor-0-3.png" alt="Floor 0 Screenshot" />
-        <img class="pc-screenshot" src="img/projects/floor-0-4.png" alt="Floor 0 Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/floor-0/Floor0SC1.webp" alt="Floor 0 Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/floor-0/Floor0SC2.webp" alt="Floor 0 Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/floor-0/Floor0SC3.webp" alt="Floor 0 Screenshot" />
+        <img class="pc-screenshot" loading="lazy" src="img/projects/floor-0/Floor0SC4.webp" alt="Floor 0 Screenshot" />
+    </div>
+    <div class="paragraph center">
+        <h3>About this game</h3>
+        First solo project in Unreal Engine (5 weeks).<br/>
+        Multi-state AI built with behaviour tree.<br/>
+        Custom built Key/Lock and item system.<br/>
+        Will leave you sleep less for days.
+    </div>
+    `, "#6C3BAA", false, false),
+    new ProjectData("swing-space", "SwingSpace", "img/projects/swing-space/SwingSpaceGIF.gif", `
+    <div class="paragraph">
+        Swing yourself up through space by grappling the planets. Get as far as you possible can and beat the highscore!
+    </div>
+    <div class="paragraph center">
+        <video class="pc-video swing-space-video" controls preload="metadata">
+            <source src="img/projects/swing-space/SwingSpaceVid.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+    </div>
+    <div class="paragraph center swing-space-shots">
+        <img class="pc-screenshot swing-space-shot" loading="lazy" src="img/projects/swing-space/SwingSpaceSC1.webp" alt="SwingSpace Screenshot" />
+        <img class="pc-screenshot swing-space-shot" loading="lazy" src="img/projects/swing-space/SwingSpaceSC2.webp" alt="SwingSpace Screenshot" />
+    </div>
+    <div class="paragraph center">
+        <h3>About this game</h3>
+        Built in Unity (5 weeks).<br/>
+        My first solo project.<br/>
+        Firebase intergration for global highscore chart.<br/>
+        Ported for mobile.<br/>
+        Awesome, but also extremely fun.
     </div>
     `, "#6C3BAA", false, false)
 ];
