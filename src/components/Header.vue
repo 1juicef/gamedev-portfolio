c2f454313eb96f8ee46d1cd83032fcf55f7baabe
1juicef <josefubaka@gmail.com>
2026-08-06 10:22:10 +0200

    feat(quick-260806-e9x): shrink and left-shift header nav labels on phone widths
    - Add font-size: 0.8em and asymmetric padding to mobile .nav-bar
    - Tighten mobile nav-link margins from 9px to 7px
    - .nav-link font-size: inherit ensures one-page buttons shrink too

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -217,11 +217,13 @@ a, .nav-link {
 @media only screen and (max-width: 620px){
   .nav-bar {
     line-height: 2em;
+    font-size: 0.8em;
+    padding: 20px 28px 20px 12px;
   }
 
   a, .nav-link {
-    margin-left: 9px;
-    margin-right: 9px;
+    margin-left: 7px;
+    margin-right: 7px;
     padding-bottom: 0px;
   }
 
