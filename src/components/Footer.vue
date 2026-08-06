dec0298422be346ff5c799177cc812fba094f712
1juicef <josefubaka@gmail.com>
2026-08-06 10:23:32 +0200

    feat(quick-260806-e9x): replace footer email line with thank-you message
    - Remove mailto address anchor and /contact link (redundant with nav + /contact page)
    - Right-hand footer line now reads 'Thanks for stopping by!'

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     
     <div class="left"><a href="https://github.com/schouffy/gamedev-portfolio" target="_blank" rel="noopener noreferrer">Portfolio</a> by Juicef <img class="footer-guy" :src="mascotSrc" alt="Running character" /></div>
-    <div class="right">Reach me at <a href="mailto:josefubaka@gmail.com">josefubaka@gmail.com</a> or <router-link to="/contact">through here</router-link></div>
+    <div class="right">Thanks for stopping by!</div>
   </div>
 </template>
 
